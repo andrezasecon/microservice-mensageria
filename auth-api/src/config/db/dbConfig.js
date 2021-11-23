d054055ac6fe2ec9f8ad653b01a8ccab24de1bb0
Andreza Secon <andrezasecon@gmail.com>
2021-11-23 18:07:36 -0300

    Implementado busca de usuário para auth_api

--- auth-api/src/config/db/dbConfig.js
+++ auth-api/src/config/db/dbConfig.js
@@ -6,21 +6,24 @@ const sequelize = new Sequelize("auth-db", "admin", "123456", {
     quoteIdentifiers: false,
     define: {
         syncOnAssociation: true,
-        timestamp: false,
+        timestamps: false,
         underscored: true,
         underscoredAll: true,
-        freezeTableName: true
-    },
-
-});
-
-sequelize.authenticate().then(() => {
-    console.log("Connection has been stablished!")
-})
-
-.catch((err) => {
-    console.error("Unable to connect to the database.");
-    console.error(err.message);
-});
-
-export default sequelize;
+        freezeTableName: true,
+      },
+      pool: {
+        acquire: 180000,
+      },
+    });
+    
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.info("Connection has been stablished!");
+      })
+      .catch((err) => {
+        console.error("Unable to connect to the database.");
+        console.error(err.message);
+      });
+    
+    export default sequelize;
