3da2d7c83c25e57368a4244223dabc803107ab98
Andreza Secon <andrezasecon@gmail.com>
2021-11-24 15:28:46 -0300

    Add token autencication para auth-api

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createInitialData } from "./src/config/db/initialData.js";
+
+import * as db from "./src/config/db/initialData.js";
 import userRoutes from "./src/modules/user/routes/UserRoutes.js";
 
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
+
+db.createInitialData();
 
 app.get("/api/status", (req, res) => {
   return res.status(200).json({
@@ -15,14 +18,8 @@ app.get("/api/status", (req, res) => {
 });
 
 app.use(express.json());
-
-app.get("/api/initial-data", (req, res) => {
-    createInitialData();
-    return res.json({ message: "Data created." });
-  });
-
 app.use(userRoutes);
 
 app.listen(PORT, () => {
-    console.info(`Server started successfully at port ${PORT}`);
-  });
+  console.info(`Server started successfully at port ${PORT}`);
+});
